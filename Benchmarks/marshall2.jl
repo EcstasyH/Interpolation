9b14dfdcb5bcdb7805e0d17d5d81e93b43cffb03
EcstasyH <wuhao164@mails.ucas.ac.cn>
2024-01-02 21:14:19 +0800

    bugs, 2 more examples

--- Benchmarks/marshall2.jl
+++ Benchmarks/marshall2.jl
@@ -1,19 +1,20 @@
 
-@polyvar x y z
+@polyvar x a b c d
 # do NOT use x0 as a variable, x0 is used as the homogenization variable
 xvars = [x]
-yvars = [y]
-zvars = [z]
+yvars = [a, b]
+zvars = [c, d]
 
 # s1 includes xvars + yvars
 # >= 0 
 s1 = [ 
-        [x^3]
+        [x^3 - a, a^3-b^2-1]
 ];
 
 # s1 includes xvars + zvars
 s2 = [  
-        [-1-x]
+        [-x-c, c^3-d^2-(1-d)^2]
+        # [-x-c, c^3-d^2-(1-d)^2]
      ];    
 
 # degree of interpolation template
--- Benchmarks/marshall3.jl
+++ Benchmarks/marshall3.jl
@@ -1,19 +1,19 @@
 
-@polyvar x y z
+@polyvar x y y0 z0
 # do NOT use x0 as a variable, x0 is used as the homogenization variable
-xvars = [x]
-yvars = [y]
-zvars = [z]
+xvars = [x, y]
+yvars = [y0]
+zvars = [z0]
 
 # s1 includes xvars + yvars
 # >= 0 
 s1 = [ 
-        [x^3]
+        [x^3 - y^2 - 1]
 ];
 
 # s1 includes xvars + zvars
 s2 = [  
-        [-1-x]
+        [-x^3 - y^2 - 1]
      ];    
 
 # degree of interpolation template
